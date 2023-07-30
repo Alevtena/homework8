1be7152c69697566d54068ac423694200b6e89e8
Alevtena <success_2006@mail.ru>
2023-07-31 01:00:19 +0300

    Add task3

--- homework8/Program.cs
+++ homework8/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿// Task 1 . Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 /* Например, задан массив:
 1 4 7 2
 5 9 2 3
@@ -48,7 +48,7 @@ Console.WriteLine("Sort array");
 SortArray(myarray);
 Show2dArray(myarray); */
 
-/* Задайте прямоугольный двумерный массив. Напишите программу, которая будет
+/* Task 2. Задайте прямоугольный двумерный массив. Напишите программу, которая будет
  находить строку с наименьшей суммой элементов.
 
 Например, задан массив:
@@ -58,7 +58,7 @@ Show2dArray(myarray); */
 5 2 6 7
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка */
 
-int[,] Create2DArray(int row, int colom)
+/* int[,] Create2DArray(int row, int colom)
 {
     int[,] array=new int [row, colom];
     for( int i=0; i<row; i++)
@@ -121,4 +121,75 @@ int [] newarray = RowsSum(myarray, row, colum);
 Console.WriteLine("Сумма элементов в каждой строке массива");
 ShowArray(newarray);
 Console.WriteLine();
-Console.Write($"Номер строки в массиве с минимальной суммой элементов {FindeIndex(newarray)}");
+Console.Write($"Номер строки в массиве с минимальной суммой элементов {FindeIndex(newarray)}"); */
+
+/* Task 3. Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18 */
+
+int[,] Create2dArray (int row, int colum)
+{
+    int [,] array = new int[row, colum];
+    for (int i=0; i<row; i++)
+        for (int j=0; j<colum; j++)
+            array[i,j] = new Random().Next(0,10);
+    return array;
+}
+
+void Show2dArray (int[,] array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        Console.WriteLine();
+    }
+}
+bool Proverka (int colum1, int row2) // проверяем условие для возможности умножения матриц
+{
+    if ( colum1==row2) 
+        return true;
+    else return false;
+}
+int[,] MultiplicateArray(int[,] firstarray , int [,] secondarray, int row1, int colum1, int row2, int colum2)
+// метод умножения матриц при выполнении условия равенства солбцов 1 матрицы = строки 2 матрицы 
+{
+    int [,] resultarray = new int[row1, colum2];
+        for (int i=0; i<row1; i++)
+            for (int j=0; j<colum2; j++)
+            {
+                int sum = 0;
+                for (int k=0; k<colum1; k++)
+                    sum += firstarray[i,k]*secondarray[k,j];
+            resultarray[i,j] = sum;
+            }
+    return resultarray; 
+}
+
+Console.Write(" Input a rows of firstarray" + " ");
+int row1 = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Input a colums of firstarray" + " ");
+int colum1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(" Input a rows of secondarray" + " ");
+int row2 = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Input a colums of secondarray" + " ");
+int colum2 = Convert.ToInt32(Console.ReadLine());
+
+int[,] firstarray = Create2dArray(row1, colum1);
+int[,] secondarray = Create2dArray(row2, colum2);
+Show2dArray(firstarray);
+Console.WriteLine();
+Show2dArray(secondarray);
+bool temp = Proverka(colum1,row2);
+if (temp==true)
+{
+    int[,] resultarray = MultiplicateArray(firstarray, secondarray, row1, colum1, row2, colum2);
+    Console.WriteLine("Произведение матриц");
+    Show2dArray(resultarray);
+}
+else Console.WriteLine("Умножение невозможно. Количество столбцов первой матрицы должно быть равно количеству строк второй матрицы");
